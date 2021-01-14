c320a649f985aea2086184f831a511e22aab93e8
spheleletholy262000@gmail.com <spheleletholy262000@gmail.com@users.noreply.github.com>
2021-01-14 17:53:57 +0200

    Added a poduct model to mapp my data source

---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Swag.IO.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Maker { get; set; }
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string[] Rating { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+        //public override string ToString()
+        //{
+        //    JsonSerializer.Serialize<Product>(this);
+        //}
+    }
+}
