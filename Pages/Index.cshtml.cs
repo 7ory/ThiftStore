43f72e46804944b693a6a14ba1c53662dac9c4e8
spheleletholy262000@gmail.com <spheleletholy262000@gmail.com@users.noreply.github.com>
2021-01-15 02:40:46 +0200

    Added data to our razor page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,24 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Swag.IO.Models;
 
 namespace Swag.IO.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileProductService productService)
         {
             _logger = logger;
+            ProductService = productService;
         }
 
         public void OnGet()
         {
-
+            Products = ProductService.GetProducts();
         }
     }
 }
